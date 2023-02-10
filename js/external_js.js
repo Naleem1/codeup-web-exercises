d2eeb09981d6719fa6b4fd3019e03d84d7556593
Najee Aleem <Naleem25@gmail.com>
2023-02-10 14:51:06 -0600

    finished exercise

--- js/external_js.js
+++ js/external_js.js
@@ -4,17 +4,23 @@
 // let userAnswer = prompt("Hey! What is your favorite color");
 // alert(`Great, ${userAnswer} is my favorite color`);
 
-
-    // When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-    // Use an alert to show the results of each problem.
-    // Finally, commit the changes to your git repository, and push to GitHub.
-
 // let movieRentals = prompt("Welcome to BlockBuster! How many movie's would you like to rent?")
 // let movieCost = 3
 // alert(`you have selected ${movieRentals} movies at $3.00 a day.`)
 // let rentalAmount = movieRentals * movieCost
 // alert(`Your total is ${rentalAmount}`)
 
+// 2nd example of movie rental
+// let priceInput = prompt("How much does a movie cost per day?");
+// let pricePerDay = parseFloat(priceInput);
+// let movie1Input = prompt("how many days did you rent little mermaid?");
+// let movie1 = parseFloat(movie1Input);
+// let movie2Input = prompt("how many days did you rent Brother Bear?");
+// let movie2 = parseFloat(movie2Input);
+// let movie3Input = prompt("how many days did you rent Hercules ?");
+// let movie3 = parseFloat(movie3Input);
+// let total = pricePerDay * (movie1 + movie2 + movie3)
+// alert(`Your total is ${total} this week`)
 
 // let job1 = prompt( "How many hours did you work for Meta this week?");
 // let job1Pay = 350;
@@ -25,9 +31,19 @@
 // let total = job1 * job1Pay + job2 * job2Pay + job3 * job3Pay
 // alert(`You have made a total of ${total} from working this week!`)
 
-// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
-let enroll = prompt("Would you like to enroll in this course, Yes or No?");
-    !confirm("are you sure?");
+// let scheduleConflicts = false;
+// let maxClassSixe = parseInt(prompt('what is the maximum size of the class'));
+// let currentClassSize = parseInt(prompt('what is the current size of the class'));
+// let canEnroll = !scheduleConflicts && (currentClassSize < maxClassSixe);
+// alert(`it is ${canEnroll} that the student can eroll in the class.`)
+
+// let minCartSize = 2;
+// let cartSize = parseInt(prompt('How many items do you have in your cart?'))
+// let premiumMember = confirm('Are you a premium member?');
+// let offerExpired = confirm('Has the offer expired?');
+// let canOfferApply = (premiumMember || (cartSize > minCartSize)) && !offerExpired;
+// alert(`It is ${canOfferApply} that the offer does apply.`)
+
 
 
 
