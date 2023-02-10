ec24c9842ba8ffe871e97f474aa56024123e3a0d
Najee Aleem <Naleem25@gmail.com>
2023-02-10 14:06:21 -0600

    need to finish last problem two problems

---
+++ js/JavaScript-with-Html.js
@@ -0,0 +1,9 @@
+"use strict";
+// let myNumber = 23;
+// console.log(myNumber)
+
+// let didConfirm = confirm("would you like to confirm")
+// console.log(`The user selected ${didConfirm}`)
+
+// let userName = prompt('What is your name?');
+// console.log(`The user's name is ${userName}`);
---
+++ js/external_js.js
@@ -0,0 +1,34 @@
+"use strict";
+// console.log("Hello from external Javascript")
+// alert("Welcome to my Awesome Website!");
+// let userAnswer = prompt("Hey! What is your favorite color");
+// alert(`Great, ${userAnswer} is my favorite color`);
+
+
+    // When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+    // Use an alert to show the results of each problem.
+    // Finally, commit the changes to your git repository, and push to GitHub.
+
+// let movieRentals = prompt("Welcome to BlockBuster! How many movie's would you like to rent?")
+// let movieCost = 3
+// alert(`you have selected ${movieRentals} movies at $3.00 a day.`)
+// let rentalAmount = movieRentals * movieCost
+// alert(`Your total is ${rentalAmount}`)
+
+
+// let job1 = prompt( "How many hours did you work for Meta this week?");
+// let job1Pay = 350;
+// let job2 = prompt( "How many hours did you work for Amazon this week?");
+// let job2Pay = 380;
+// let job3 = prompt( "How many hours did you work for Google this week?");
+// let job3Pay = 400;
+// let total = job1 * job1Pay + job2 * job2Pay + job3 * job3Pay
+// alert(`You have made a total of ${total} from working this week!`)
+
+// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+let enroll = prompt("Would you like to enroll in this course, Yes or No?");
+    !confirm("are you sure?");
+
+
+
+
