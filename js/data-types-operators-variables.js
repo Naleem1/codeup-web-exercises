5fa4c0b39838cb3c70bfd58207c4e2f9eea84a31
Najee Aleem <Naleem25@gmail.com>
2023-02-10 10:15:55 -0600

    reviewed and updated exercise

--- js/data-types-operators-variables.js
+++ js/data-types-operators-variables.js
@@ -108,11 +108,7 @@
 //Answer = 27
 
 //6.
-// let companies = ["Google","Amazon","Facebook"]
-// let Gpay = 400
-// let Apay = 380
-// let Fpay = 350
-//
+
 // let fbTotal = 350 * 10
 // let gTotal = 400 * 6
 // let aTotal = 380 * 4
@@ -120,26 +116,35 @@
 // Answer = 7,420
 //7.
 // A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule
-// let classFull = false
-// let classScheduleConflict = false
-// let youAreEnrolled = classFull && classScheduleConflict
-// console.log(youAreEnrolled)
+// let scheduleConflicts = false;
+// let classSize = 30;
+// let currentClassSize = 24;
+// let canEnroll = !scheduleConflicts && currentClassSize <= classSize
+// console.log(canEnroll)
 
 // 8.
-// let productOffer = 2
-// let productOfferExpired= false
-// let premiumMember = true
-// let productOfferApplied = productOffer && productOfferExpired || premiumMember
-// console.log(productOfferApplied)
+// let minCartSize = 2;
+// let cartSize = 3;
+// let premiumMember = false;
+// let offerExpired = false;
+// let canOfferApply = (premiumMember || (cartSize >= minCartSize)) && !offerExpired
+// console.log(canOfferApply)
 
 // 9.
-// let username = 'codeup';
-// let password = 'notastrongpassword';
-//
-// let passwordConditions = password.length >= 5 && password != username
-// let userNameConditions = username.length <= 20 && username != ""
-//
-// console.log(passwordConditions)
+let username = 'codeup';
+let password = 'notastrongpassword';
 
+let minChar = password.length >= 5;
+let noUserName = password.includes(username);
+let maxUserChar = username.length <= 20;
 
+let userLength = username.length;
+let passLength = password.length;
+let firstCharUser = username.substring(0,1);
+let lastCharUser = username.substring((userLength -1), userLength);
+let firstCharPass = password.substring(0,1);
+let lastCharPass = password.substring((passLength -1), passLength);
+
+let hasWhiteSpace = firstCharUser === ' ' || lastCharUser === ' ' || firstCharPass === ' ' || lastCharPass === ' ';
+console.log(hasWhiteSpace)
 
